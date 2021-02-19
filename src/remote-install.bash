836ff8646066e05d4a8e90d2cf4ade62654e1a82
Samuel K <62254492+ExtinctAxolotl@users.noreply.github.com>
2021-02-19 11:07:19 +0100

    Maybe this will fix it

--- src/remote-install.bash
+++ src/remote-install.bash
@@ -49,8 +49,8 @@ else
 fi
 
 info "Cloning git repo...\n"
-mkdir -p "$ALIEN_DIR_NAME"
-git clone "$BASE_REPO_URL.git" "$ALIEN_DIR_NAME"
+mkdir -p $ALIEN_DIR_NAME
+git clone $BASE_REPO_URL.git $ALIEN_DIR_NAME
 info "Installing..."
 cd "$ALIEN_DIR_NAME"
 ./src/install.bash
