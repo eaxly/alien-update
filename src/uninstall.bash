9744affadc0441515262ae0fe5bbf021fab8f0f4
Samuel K <mrsosaki@posteo.net>
2021-04-10 15:08:09 +0100

    Update some formatting in src/uninstall.bash

--- src/uninstall.bash
+++ src/uninstall.bash
@@ -20,7 +20,7 @@ success() {
   echo -en "${GREEN}${*}${NC}"
 }
 
-read -p "Are you sure you want to uninstall Alien? [y/N] " -n 1 answer
+read -p "Are you sure you want to uninstall Alien? [y/n] " -n 1 answer
 echo
 if [ $answer != "y" ];then
   info "Exiting...\n"
@@ -37,17 +37,17 @@ sudo rm -r $HOME/.alien-update
 echo
 sleep 1
 
-read -p "Do you want to remove the Configuration file? [y/N] " -n 1 answer
+read -p "Do you want to remove the Configuration file? [y/n] " -n 1 answer
 echo
 if [ $answer != "y" ];then
-  info "Configuration File will be kept in $HOME/.config/alien-config.bash"
+  info "Configuration File will be kept in $HOME/.config/alien-config.bash\n"
   sleep 1
 else
   info "Removing Configuration file...\n"
-rm $HOME/.config/alien-config.bash
+  rm $HOME/.config/alien-config.bash
 fi
 sleep 1
 
 info "Removing Desktop file...\n"
 rm $HOME/.local/share/applications/alien.desktop
-success "\nSuccessfully uninstalled alien!\nWe will miss you!"
+success "\nSuccessfully uninstalled alien!\nWe will miss you! D:"
